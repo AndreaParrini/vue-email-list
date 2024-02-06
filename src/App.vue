9beb9a99c02cc61214cf7f9c9416cdfa0ecfa7dc
AndreaParrini <andrea.parrini26@gmail.com>
2024-02-06 17:58:25 +0100

    add reset role and modify for loop

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
     }
   },
   mounted() {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
           console.log(response.data.response);
